55cc1ba6d274befe3c34ce3611d60c9b1a4a181d
pohyongzhi <pohyongzhi@gmail.com>
2023-01-01 15:51:46 +0800

    Added Player Class

--- project.py
+++ project.py
@@ -1,9 +1,5 @@
 # This program is a game of BlackJack where the player is able to versus the computer
 
-# Class to represent a deck of cards, including methods to shuffle the deck and deal a card to a player.
-
-# Create a class to represent a player, including methods to receive a card and calculate the value of their hand.
-
 # Write a main game loop that allows the player to play multiple rounds of Blackjack.
 
 # Implement the rules of the game, including when the dealer must hit or stand and when the player wins, loses, or pushes (ties with the dealer).
@@ -25,7 +21,26 @@ if reject more cards say stay
 if <16 need take more cards
 """
 
-import random
+import random, sys
+
+# Main function
+def main():
+    deck = Deck()
+    
+    john = Player()
+    card = deck.deal_card()
+    john.receive_card(card)
+    card = deck.deal_card()
+    john.receive_card(card)
+    card = deck.deal_card()
+    john.receive_card(card)
+    print(john.show__hand())
+
+    print(john.calculate_hand())
+
+    # Get users choice
+    # menu_selection()
+
 
 # Class to represent a single playing card
 class Card:
@@ -51,7 +66,7 @@ class Card:
         """
         This method prints the value and suit of the card.
 
-        Returns:
+        returns:
             self.suit: suit of the card.
             self.value: Value of the card.
         """
@@ -80,18 +95,13 @@ class Deck:
         """
         This method populate the empty deck of cards.
         """
-        suits = {
-            "spade": "♠️",
-            "heart": "♥️",
-            "diamond": "♦️",
-            "club": "♣️"
-        }
 
-        cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        suits = ["♠️", "♥️", "♦️", "♣️"]
+        values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-        for count, suit in enumerate(suits):
-            for card in cards:
-                self.cards_in_deck.append(Card(suits[suit], card))
+        for suit in suits:
+            for card in values:
+                self.cards_in_deck.append(Card(suit, card))
 
     def return_cards(self):
         """
@@ -121,30 +131,147 @@ class Deck:
         """
         card = self.cards_in_deck.pop(0)
 
-        return card
+        return "{}".format(card)
         
     def count_cards(self):
         """
         This method prints the number of cards in the deck.
 
-        Returns:
+        returns:
             len(self.cards_in_deck)
         """
         return "{}".format(len(self.cards_in_deck))
 
 
-def main():
-    # Initialise the deck
-    deck = Deck()
-    print(deck.deal_card())
-    # Return the list of cards by calling the deck.return_cards() method
-    card_deck = deck.return_cards()
+# Class to represent a player
+class Player:
+    """
+    This class represent a player
 
-    print(deck.count_cards())
-    # # Iter over the list and print out the cards, as just printing the whole list will return the object memory location as python calls "__repr__" method instead of "__str__"
-    # for card in card_deck:
-    #     print(card)
- 
+    Attributes:
+            card_in_hand (list): A list of total cards on hand.    
+    """
+    def __init__(self):
+        """
+        The constructor for Player class.
+
+        Parameters:
+            card_in_hand (list): A list of total cards on hand.
+        """
+        self.card_in_hand = []
+
+    def receive_card(self, card):
+        """
+        This method receive a card given by deal_card in Deck class.
+        """
+        self.card_in_hand.append(card)
+
+    def calculate_hand(self):
+        """
+        This method calculates the total value in the player's hand.
+        """
+        face_value_list = ["2", "3", "4", "5", "6", "7", "8", "9", "10"]
+        picture_list = ["J", "Q", "K"]
+        total_value = 0
+        ace_count = 0
+
+        # Check if ACE is in the hand
+        for card in self.card_in_hand:
+            # Remove all symbol of J, Q, K, A, and space
+            card = card.rstrip("♠️♥️♦️♣️ ")
+            
+            if card == "A":
+                ace_count += 1
+
+        # Loop through the whole hand and check if card is any value other than ACE
+        for card in self.card_in_hand:
+            # Remove all symbol of J, Q, K, A, and space
+            card = card.rstrip("♠️♥️♦️♣️ ")
+
+            # Check if card is in face value list
+            if card in face_value_list:
+                total_value += int(card)
+
+            # Check if card is in picture value list
+            elif card in picture_list:
+                total_value += 10
+
+            # Check if card is ACE
+            else:
+                if total_value > 21 and ace_count > 1:
+                    total_value += 1
+                    ace_count -= 1
+                else:
+                    total_value += 11
+
+        return total_value
+
+    def show__hand(self):
+        """
+        This method shows all the card in the player's hand.
+
+        returns:
+            self.card_in_hand = A list of cards that is given to the player.
+        """
+        return self.card_in_hand
+
+
+# Function to check if user input is value
+def menu_selection():
+    """
+    Takes in the user input and check if value is in int
+
+    returns:
+        choice (int): The string value given by the user, converted to int
+    """
+    while True:
+        try:
+            # Show menu options
+            print("Choose 1 to start the BlackJack game")
+            print("Choose 2 to show the rules of BlackJack")
+            print("Choose 3 to exit")
+            choice = input("Enter the choice in integer: ")
+            choice = int(choice)
+
+            # Data validation
+            if choice == 1:
+                ...
+            elif choice == 2:
+                show_rules()
+            else:
+                sys.exit("You have exited the program!")
+
+        except ValueError:
+            print("Choice must be an integer and between value of 1-3!")
+            pass
+
+
+# Function to show the rules of BlackJack
+def show_rules():
+    """
+    Function to print out the rules of BlackJack.
+    """
+    print("\n")
+    print("Blackjack is a popular card game played with a deck of 52 cards. The goal of the game is to beat the dealer by having a hand value that is closer to 21 than the dealer's hand, without going over 21.")
+    print("\n")
+    print("Here are the basic rules of blackjack:")
+    print("1. The game is played with one deck of 52 cards.")
+    print("2. The value of each card is as follows:")
+    print("    - 2 through 10 are worth their face value.")
+    print("    - Jack, Queen, and King are worth 10 points.")
+    print("    - Ace is worth 1 or 11 points, depending on which value would be more beneficial for the hand.")
+    print("3. The dealer deals two cards to each player and two cards to themselves, with one of the dealer's cards being face up and the other face down (the 'hole' card).")
+    print("4. Each player's turn begins with the player to the left of the dealer. During their turn, the player has the following options:")
+    print("    - Hit: Take another card from the deck.")
+    print("    - Stand: Keep their current hand and end their turn.")
+    print("5. If a player's hand value goes over 21, they lose the game (this is called a 'bust').")
+    print("6. Once all players have completed their turns, the dealer reveals their hole card and hits or stands based on the following rules:")
+    print("    - If the dealer has a hand value of 17 or higher, they must stand.")
+    print("    - If the dealer has a hand value of 16 or lower, they must hit.")
+    print("7. If the dealer busts, all remaining players win the game. If the dealer does not bust, the player's hands are compared to the dealer's hand and the player with the hand closest to 21 without going over wins the game.")
+    print("8. If a player and the dealer have the same hand value, the game is a tie (this is called a 'push').")
+    print("\n")
+
 
 if __name__ == "__main__":
     main()
